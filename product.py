bacb01c271af651ca2d3455ec0fd798c6b3852b7
YC Chen <chency1216@gmail.com>
2019-07-24 00:58:47 +0800

    有讀取寫入的功能

--- product.py
+++ product.py
@@ -1,4 +1,18 @@
+#讀取檔案
 product = []
+with open('product.csv', 'r', encoding = 'utf-8') as f:
+	for line in f :
+		if '商品,價格' in line:
+			continue
+		s = line.strip().split(',')
+		product.append(s)
+	'''
+    上面取出資料並忽略表頭的快寫法
+	product = [line.strip().split(',') for line in f if '商品,價格'  not in line ]
+	'''
+print(product)	
+
+#輸入新的資料
 while True:
 	name = input('請輸入商品名稱:')
 	if name == 'q':
@@ -7,6 +21,11 @@ while True:
 	product.append([name, price])
 print(product)
 
+#印出所有商品與價格
+for p in product:
+	print(p[0], '的價格為', [1])
+
+#將輸入的資料寫入檔案
 with open('product.csv', 'w', encoding='utf-8') as f:
 	f.write('商品,價格\n') 
 	for p in product:
